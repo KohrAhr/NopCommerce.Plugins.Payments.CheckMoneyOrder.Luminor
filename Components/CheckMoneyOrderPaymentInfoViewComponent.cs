b1cd95995b4266d74477794d841b4af2962eadda
MonsterZam <github@1click.lv>
2023-11-23 17:27:59 +0200

    Initial commit, based on standard CheckMoneyOrder component

---
+++ Components/CheckMoneyOrderPaymentInfoViewComponent.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Plugin.Payments.CheckMoneyOrder.Luminor.Models;
+using Nop.Services.Localization;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Payments.CheckMoneyOrder.Luminor.Components
+{
+    [ViewComponent(Name = "CheckMoneyOrder.Luminor")]
+    public class CheckMoneyOrderViewComponent : NopViewComponent
+    {
+        private readonly CheckMoneyOrderLuminorPaymentSettings _checkMoneyOrderPaymentSettings;
+        private readonly ILocalizationService _localizationService;
+        private readonly IStoreContext _storeContext;
+        private readonly IWorkContext _workContext;
+
+        public CheckMoneyOrderViewComponent(CheckMoneyOrderLuminorPaymentSettings checkMoneyOrderPaymentSettings,
+            ILocalizationService localizationService,
+            IStoreContext storeContext,
+            IWorkContext workContext)
+        {
+            _checkMoneyOrderPaymentSettings = checkMoneyOrderPaymentSettings;
+            _localizationService = localizationService;
+            _storeContext = storeContext;
+            _workContext = workContext;
+        }
+
+        /// <returns>A task that represents the asynchronous operation</returns>
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var store = await _storeContext.GetCurrentStoreAsync();
+
+            var model = new PaymentInfoModel
+            {
+                DescriptionText = await _localizationService.GetLocalizedSettingAsync(_checkMoneyOrderPaymentSettings,
+                    x => x.DescriptionText, (await _workContext.GetWorkingLanguageAsync()).Id, store.Id)
+            };
+
+            return View("~/Plugins/Payments.CheckMoneyOrder.Luminor/Views/PaymentInfo.cshtml", model);
+        }
+    }
+}
